fa3a885b8ae4b08be5b5a570090a31178b24fa60
techyNonso <williamikeji@gmail.com>
2023-09-29 01:39:14 +0100

    download of zip file done

--- src/app/providers.js
+++ src/app/providers.js
@@ -10,36 +10,36 @@ import {
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet, polygon, filecoinCalibration } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
-const lilypad = {
-  id: 1337,
-  name: "Lilypad Lalechuza testnet",
-  network: "Lilypad Lalechuza testnet",
-  iconUrl: "",
-  iconBackground: "#fff",
-  nativeCurrency: {
-    decimals: 18,
-    name: "Lilypad Lalechuza testnet",
-    symbol: "ETH",
-  },
-  rpcUrls: {
-    public: { http: ["http://testnet.lilypadnetwork.org:8545"] },
-    default: { http: ["http://testnet.lilypadnetwork.org:8545"] },
-  },
-  // blockExplorers: {
-  //   default: { name: "SnowTrace", url: "https://snowtrace.io" },
-  //   etherscan: { name: "SnowTrace", url: "https://snowtrace.io" },
-  // },
-  // contracts: {
-  //   multicall3: {
-  //     address: "0xca11bde05977b3631167028862be2a173976ca11",
-  //     blockCreated: 11_907_934,
-  //   },
-  // },
-  testnet: true,
-};
+// const lilypad = {
+//   id: 1337,
+//   name: "Lilypad Lalechuza testnet",
+//   network: "Lilypad Lalechuza testnet",
+//   iconUrl: "",
+//   iconBackground: "#fff",
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: "Lilypad Lalechuza testnet",
+//     symbol: "ETH",
+//   },
+//   rpcUrls: {
+//     public: { http: ["http://testnet.lilypadnetwork.org:8545"] },
+//     default: { http: ["http://testnet.lilypadnetwork.org:8545"] },
+//   },
+//   // blockExplorers: {
+//   //   default: { name: "SnowTrace", url: "https://snowtrace.io" },
+//   //   etherscan: { name: "SnowTrace", url: "https://snowtrace.io" },
+//   // },
+//   // contracts: {
+//   //   multicall3: {
+//   //     address: "0xca11bde05977b3631167028862be2a173976ca11",
+//   //     blockCreated: 11_907_934,
+//   //   },
+//   // },
+//   testnet: true,
+// };
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, filecoinCalibration, lilypad],
+  [mainnet, polygon, filecoinCalibration],
   [publicProvider()]
 );
 
