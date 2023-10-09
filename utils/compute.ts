c85ea16edd43dc4162b85f25a3c57c820c4f7bfa
Hiro <laciferin@gmail.com>
2023-10-09 11:21:28 +0530

    feat; compute
    Took 3 minutes
    
    
    Took 31 seconds

---
+++ utils/compute.ts
@@ -0,0 +1,28 @@
+
+import axios from 'axios'
+
+let data = {
+    "Engine": "Docker",
+    "Docker": {
+        "Image": "ubuntu",
+        "Entrypoint": ["echo"],
+        "Parameters": ["hello"]
+    },
+    "Deal": {
+        "Concurrency": 1
+    },
+    "Verifier": "Noop",
+    "PublisherSpec": {
+        "Type": "IPFS"
+    }
+};
+
+
+
+axios.post('http://dashboard.bacalhau.org:1000/api/v1/run', data)
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
