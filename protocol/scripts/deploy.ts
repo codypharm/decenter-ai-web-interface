75a95e368bb04673ecaa7a8bbd89f1030394dcfb
techyNonso <williamikeji@gmail.com>
2024-01-23 09:15:58 +0100

    contract compilation and deployment done

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -1,21 +1,22 @@
 import { formatEther, parseEther } from "viem";
 import hre from "hardhat";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = BigInt(currentTimestampInSeconds + 60);
 
-  const lockedAmount = parseEther("0.001");
-
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  console.log(
-    `Lock with ${formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const  treasury = process.env.TREASURY
+   const stableCoin = await hre.viem.deployContract("Token");
+    const dcen = await hre.viem.deployContract("Dcen");
+    const presale = await hre.viem.deployContract("Presale", [stableCoin.address,treasury,parseEther('1','wei'),parseEther('1','wei')]);
+   
+    
+    console.log("Stable Coin Address:", stableCoin.address)
+    console.log("Dcen token Address:", dcen.address)
+    console.log("Presale contract Address:", presale.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
